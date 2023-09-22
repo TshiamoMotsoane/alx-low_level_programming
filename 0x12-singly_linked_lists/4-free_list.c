65c082e81adc2249e088dd8d4fd107c8b7d7a671
TshiamoMotsoane <motsoanetshiamo@outlook.com>
2023-09-22 13:13:57 -0700

    Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - Free a list_t list
+ * @head: pointer to the head of the list
+ *
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}
