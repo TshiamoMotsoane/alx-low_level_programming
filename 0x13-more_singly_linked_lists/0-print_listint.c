e9244cfa627a16a4bec7e86f8396d31a96715b80
TshiamoMotsoane <motsoanetshiamo@outlook.com>
2023-09-26 06:10:36 -0700

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,12 +11,11 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
-	const listint_t *current = h;
 
-	while (current != NULL)
+	while (h != NULL)
 	{
-		printf("%d\n", current->n);
-		current = current->next;
+		printf("%d\n", h->n);
+		h = h->next;
 		count++;
 	}
 
