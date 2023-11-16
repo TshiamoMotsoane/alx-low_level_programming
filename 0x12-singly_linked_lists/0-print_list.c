17ab1c01591d5e4bb77a35c2e3d6fc7b4476e54d
TshiamoMotsoane <motsoanetshiamo@outlook.com>
2023-11-16 04:09:23 -0800

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,12 +14,12 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (!h->str)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
-		h = h->next;
 		nodes++;
+		h = h->next;
 	}
 	return (nodes);
 }
