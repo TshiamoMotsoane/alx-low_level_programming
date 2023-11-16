c4e36dad8fec5532e1af5aff4f472ebbeedb56e7
TshiamoMotsoane <motsoanetshiamo@outlook.com>
2023-11-16 04:03:33 -0800

    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
 #ifndef LINKED
-#define LINKE
+#define LINKED
 
 #include <stddef.h>
 #include <string.h>
@@ -25,3 +25,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+
+#endif
