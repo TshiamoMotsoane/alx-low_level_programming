a707b93bfa65f4fa07a1fd223438c10a52e0c72c
TshiamoMotsoane <motsoanetshiamo@outlook.com>
2023-11-23 06:56:15 -0800

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -20,11 +20,11 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] < '0' || b[i] > '1')
+		if (b[i] < '0' && b[i] > '1')
 			return (0);
 
 		num <<= 1;
-		if (b[i] < '1')
+		if (b[i] == '1')
 			num += 1;
 	}
 	return (num);
