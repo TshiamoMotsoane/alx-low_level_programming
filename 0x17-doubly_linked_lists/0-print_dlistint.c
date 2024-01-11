3a9b7d9e5255fd8c4f1045a156bb952d1b533e59
TshiamoMotsoane <motsoanetshiamo@outlook.com>
2024-01-11 05:08:00 -0800

    Print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -12,7 +12,13 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t nodes = 0;
 
-	while (h)
+	if (h == NULL)
+		return (nodes);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
