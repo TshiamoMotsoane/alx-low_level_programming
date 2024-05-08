50367461a90948d81c18a4625173d02940ab3ee3
TshiamoMotsoane <motsoanetshiamo@outlook.com>
2024-05-08 10:15:41 -0700

    created list

---
+++ 0x1E-search_algorithms/create_list.c
@@ -0,0 +1,33 @@
+#include "../search_algos.h"
+#include <stdlib.h>
+
+void free_list(lisint_t *list);
+
+/**
+ * create_list - creates singly linked list
+ * @array: pointer to the array to fill the list
+ * @size: size of the array
+ *
+ * Return: a pointer to the head of the created list (NULL on failure)
+ */
+listint_t *create_list(int *array, size_t size)
+{
+	listint_t *list;
+	listint_t *node;
+
+	list = NULL;
+	while (array && size--)
+	{
+		node = malloc(sizeof(*node));
+		if (!node)
+		{
+			free_list(list);
+			return (NULL);
+		}
+		node->n = array[size];
+		node->index = size;
+		node->next = list;
+		list =node;
+	}
+	return (list);
+}
