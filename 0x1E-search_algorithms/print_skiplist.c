968618e51b24e715966d45342acd18154e4d5049
TshiamoMotsoane <motsoanetshiamo@outlook.com>
2024-05-08 10:20:26 -0700

    Print skiplist

---
+++ 0x1E-search_algorithms/print_skiplist.c
@@ -0,0 +1,24 @@
+#include "../search_algos.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_skiplist - dump the content of a skiplist_t
+ * @list: pointer to the head of the list
+ */
+void print_skiplist(const skiplist_t *list)
+{
+	const skiplist_t *node;
+
+	printf("List :\n");
+	for (node = list; node; node = node->next)
+	{
+		printf("Index[%lu] = [%d]\n", node->index, node->n);
+	}
+	printf("\nExpress lane :\n");
+	for (node = list; node; node = node->express)
+	{
+		printf("Index[%lu] = [%d]\n", node->index, node->n);
+	}
+	printf("\n");
+}
